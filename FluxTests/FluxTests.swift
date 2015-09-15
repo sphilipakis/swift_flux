357adf83aa81a46cbc53892002aaa52188a27d09
spsadmin <spsadmin@stephanes-iMac.local>
2015-09-14 23:25:34 -0700

    Working on tests

--- FluxTests/FluxTests.swift
+++ FluxTests/FluxTests.swift
@@ -11,17 +11,6 @@ import XCTest
 
 var MainDispatcher:Dispatcher!
 
-class TestStore : BaseStore {
-    var expectation : XCTestExpectation!
-    override init() {
-        super.init()
-        register(MainDispatcher) {
-            action in
-            print("Action : \(action)")
-            self.expectation.fulfill()
-        }
-    }
-}
 
 class FluxTests: XCTestCase {
     
@@ -39,24 +28,49 @@ class FluxTests: XCTestCase {
     }
     
     func testStoreRegister() {
-        let testStore = TestStore()
-        let token = testStore.registrationToken
+        
+        class MyStore : BaseStore {
+            
+        }
+        
+        let theStore = MyStore()
+
+        theStore.register(MainDispatcher) {
+            action in
+            print("Action : \(action)")
+        }
+        
+        let token = theStore.registrationToken
         
         XCTAssert(token != nil, "Store should be registered with a registration token")
         XCTAssert(MainDispatcher.stores.count == 1, "Dispatcher should have one store")
-        XCTAssert((MainDispatcher.stores[token!] === testStore), "Store should be the same in dispatcher")
+        XCTAssert((MainDispatcher.stores[token!] === theStore), "Store should be the same in dispatcher")
 
-        MainDispatcher.unregister(testStore)
+        MainDispatcher.unregister(theStore)
         XCTAssert(MainDispatcher.stores.count == 0, "Dispatcher should have zero store")
-        XCTAssert(testStore.registrationToken == nil, "Store should not have any registration token anymore")
+        XCTAssert(theStore.registrationToken == nil, "Store should not have any registration token anymore")
         XCTAssert(MainDispatcher.stores[token!] == nil, "Token should be un assigned in dispatcher")
     }
     
-    func testExample() {
-        let expectation = expectationWithDescription("testStore should run handle")
-        let action = Action(name:"test",properties:["aaa":"bbb"])
-        var testSore = TestStore()
-        testSore.expectation = expectation
+    func testDispatch() {
+        class MyStore : BaseStore {
+        }
+        let expectation = expectationWithDescription("store should run handle")
+        let action = Action(name:"test",properties:["theKey":"the value"])
+        var testSore = MyStore()
+        testSore.register(MainDispatcher) {
+            action in
+            if action.name != "test" {
+                XCTFail("action name should be 'test'")
+            } else {
+                if "the value" == action.properties["theKey"] as? String {
+                    expectation.fulfill()
+                    return
+                }
+                XCTFail("action properties[theKey] should be the value")
+            }
+        }
+        
         dispatch_async(dispatch_get_main_queue()) {
             MainDispatcher.dispatch(action)
         }
@@ -66,7 +80,43 @@ class FluxTests: XCTestCase {
                 print("Error \(error)")
             }
         }
+    }
+    
+    
+    func testWaitFor() {
+        class StoreA : BaseStore {
+            var state : String? = nil
+        }
+        class StoreB : BaseStore {
+        }
         
+        let expectationB = expectationWithDescription("storeB should handle the action after A")
+        let action = Action(name:"test",properties:["theKey":"the value"])
+        let storeA = StoreA()
+        let storeB = StoreB()
+        storeA.register(MainDispatcher) {
+            action in
+            storeA.state = "Done"
+        }
+        storeB.register(MainDispatcher) {
+            action in
+            MainDispatcher.waitFor([storeA.registrationToken!])
+            if storeA.state == "Done" {
+                expectationB.fulfill()
+            } else {
+                XCTFail("storeA should have handled the action first")
+            }
+        }
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            MainDispatcher.dispatch(action)
+        }
+        waitForExpectationsWithTimeout(10) {
+            error in
+            if let error = error {
+                print("Error \(error)")
+            }
+        }
     }
     
     func testPerformanceExample() {
